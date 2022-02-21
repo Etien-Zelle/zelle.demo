6f98f885b209fd83a002820a2ef435bc5d570758
Linda <etienmbah@gmail.com>
2022-02-21 17:58:25 +0000

    modified

--- security.sh
+++ security.sh
@@ -8,6 +8,7 @@ then
 echo "code is validated"
 echo "Grant system access"
 else
+echo "EXCERCISE CAUTION"
 echo "Security risk, code RED"
 echo "Deny access and shut all system servers. Viberate military alarm and alert FBI"
 fi
